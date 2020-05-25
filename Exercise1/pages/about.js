428dccc63f7945cd6632e428ddd351f3f1564f98
sdfsdf <sdfsdfsdf@dddd.com>
2020-05-25 16:17:57 +0300

    Exercise 1 added

---
+++ Exercise1/pages/about.js
@@ -0,0 +1,9 @@
+
+
+export default function About(){
+
+    return <>
+        <h1>About Page</h1>
+        <p>React NextJS App is fun.</p>
+    </>
+}
---
+++ Exercise1/pages/index.js
@@ -0,0 +1,8 @@
+
+export default function HomePage(){
+
+    return <div>
+        <h1>NextJS App!</h1>
+       <p>This is a sample react app.</p>
+    </div>
+}
